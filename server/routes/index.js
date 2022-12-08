b777129aabf929e3ad0b58b37ba7ed9c821a5803
DogKingFun <108065335+DogKingFun@users.noreply.github.com>
2022-12-08 19:27:31 +0900

    Update index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,31 +1,9 @@
 var express = require('express');
-var MongoClient = require('mongodb').MongoClient;
-var ObjectId = require('mongodb').ObjectID;
 var router = express.Router();
 
-const URL = 'mongodb://username:password@mongo/test?authSource=admin';
-const client = new MongoClient(URL);
-
-(async() => {
-  try {
-    await client.connect();
-    console.log('Succesfully connected to mongo');
-  } catch (e) {
-    console.error(e);
-  }
-})();
-
-const db = client.db();
-const col = db.collection('test-collection');
-var id = new ObjectId();
-col.insertOne({_id:id,n:0});
-
 /* GET home page. */
-router.get('/', async function(req, res, next) {
-  col.updateOne({_id:id},{$inc:{n:1}});
-  result = await col.findOne({_id:id});
-  console.log(result);
-  res.render('index', { title: 'Express' +result['n'] });
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express'});
 });
 
 module.exports = router;
