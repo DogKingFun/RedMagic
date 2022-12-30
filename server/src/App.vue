c98e694cdcd4f206736e6e04bfa3862a4f30bc21
DogKingFun <108065335+DogKingFun@users.noreply.github.com>
2022-12-30 00:27:00 +0000

    horizontal Viewの追加

--- server/src/App.vue
+++ server/src/App.vue
@@ -3,6 +3,7 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link> |
     <router-link to="/flow">Flow</router-link>
+    <router-link to="/flow2">Flow2</router-link>
   </nav>
   <router-view/>
 </template>
---
+++ server/src/views/FlowView2.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { Background, Controls, MiniMap, Panel, PanelPosition } from '@vue-flow/additional-components'
+import { VueFlow, isNode, useVueFlow } from '@vue-flow/core'
+import { ref } from 'vue'
+import { initialElements } from '../assets/javascripts/initial-elements2.js'
+
+const { onConnect, addEdges } = useVueFlow()
+
+const elements = ref(initialElements)
+
+onConnect((params) => addEdges([params]))
+</script>
+
+<template>
+  <VueFlow
+    v-model="elements"
+    class="basicflow"
+    :default-edge-options="{ type: 'smoothstep' }"
+    :default-zoom="1.5"
+    :min-zoom="0.2"
+    :max-zoom="4"
+    fit-view-on-init
+  >
+    <Background pattern-color="#aaa" gap="8" />
+
+    <MiniMap />
+
+    <Controls />
+  </VueFlow>
+</template>
+
+<style>
+  @import '../assets/stylesheets/main2.css';
+</style>
